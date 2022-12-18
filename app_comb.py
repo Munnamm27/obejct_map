f4607d82212614368d72b6e2638f2ee4ef5aa38e
Mahmud Hasan Munna <64007165+Munnamm27@users.noreply.github.com>
2022-12-18 18:08:45 +0600

    Update app_comb.py

--- app_comb.py
+++ app_comb.py
@@ -90,7 +90,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
-
+server=app.server
 
 app.layout = dbc.Container([
 
